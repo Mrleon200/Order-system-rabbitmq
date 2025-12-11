f572e78e72070782b9301a2bdcf8b352898383bf
Mrleon200 <163148950+Mrleon200@users.noreply.github.com>
2025-12-11 17:14:49 +0700

    Delete src/main/java/com/nhom2 directory

--- src/main/java/com/nhom2/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhom1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nhom1.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
